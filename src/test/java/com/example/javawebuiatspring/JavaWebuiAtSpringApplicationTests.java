0631affc333df9a06c05f69d2130dfacbfcbbc3f
Anton Yaroslavsev <yaroslavsev@mail.ru>
2020-10-12 20:41:52 +0500

    Домашнее задание 4

---
+++ src/test/java/com/example/javawebuiatspring/JavaWebuiAtSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javawebuiatspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebuiAtSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
